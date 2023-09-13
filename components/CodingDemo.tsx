7763bd6ef6127efc2ac225eadf9ec5cadd9e5092
unknown <devendra13.dc@gmail.com>
2023-09-13 23:33:52 +0545

    adding code

--- components/CodingDemo.tsx
+++ components/CodingDemo.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 const CodingDemo = () => {
@@ -7,6 +8,7 @@ const CodingDemo = () => {
 
   const divstyle = ["w-full flex items-center justify-center mt-[10px]"];
   const imgStyle = ["w-full flex items-center justify-center mt-[10px]"];
+  const description = "Let's make it interactive. See the magic of tailwind.";
 
   useEffect(() => {
     let currentIndex = 0;
@@ -47,34 +49,96 @@ const CodingDemo = () => {
   }, [text1, text2, imgStyle, divstyle]);
 
   return (
-    <div className="">
-      <div>
-        <code className="w-[700px] flex flex-col items-start bg-black p-[20px] text-white rounded-xl">
-          <span>
-            <span>{"<div"}</span>
-            <span> </span>
-            <span className="text-primaryBlue">className</span>
-            <span>{`=`}</span>
-            <span>{`'`}</span>
-            <span className="text-amber-500">{text1}</span>
-            <span>{`'`}</span>
-            <span>{">"}</span>
-          </span>
+    <div className="w-full flex justify-evenly gap-[50px]">
+      <code className="min-w-[55%] max-w-[55%] flex flex-col items-start bg-black p-[20px] text-white rounded-xl">
+        <span>
+          <span>{"<div"}</span>
+          <span> </span>
+          <span className="text-primaryBlue">className</span>
+          <span>{`=`}</span>
+          <span>{`'`}</span>
+          <span className="text-amber-500">{text1}</span>
+          <span>{`'`}</span>
+          <span>{">"}</span>
+        </span>
 
-          <span className="ml-[20px]">
-            <span>{"<Image"}</span>
-            <span> </span>
-            <span className="text-primaryBlue">{"className"}</span>
-            <span>{`=`}</span>
-            <span>{`'`}</span>
-            <span className="text-amber-500">{text2}</span>
-            <span>{`'`}</span>
-          </span>
+        <span className="ml-[20px]">
+          <span>{"<Image"}</span>
+          <span> </span>
+          <span className="text-primaryBlue">{"className"}</span>
+          <span>{`=`}</span>
+          <span>{`'`}</span>
+          <span className="text-amber-500">{text2}</span>
+          <span>{`'`}</span>
+          <br />
+          <span className="text-primaryBlue ml-[30px]">{"src"}</span>
+          <span>{`=`}</span>
+          <span>{`'`}</span>
           <span>
-            
+            {"/devendra.png"}
           </span>
-          <span>{"</div>"}</span>
-        </code>
+          <span>{"'"}</span>
+          <span>{" "}</span>
+          <span className="text-primaryBlue ml-[5px]">{"alt"}</span>
+          <span>{`=`}</span>
+          <span>{`'`}</span>
+          <span>
+            {"image"}
+          </span>
+          <span>{"'"}</span>
+          <br />
+          <span className="text-primaryBlue ml-[30px]">{"width"}</span>
+          <span>{`=`}</span>
+          <span>{`'`}</span>
+          <span>
+            {"100"}
+          </span>
+          <span>{"'"}</span>
+          <span>{" "}</span>
+          <span className="text-primaryBlue ml-[5px]">{"height"}</span>
+          <span>{`=`}</span>
+          <span>{`'`}</span>
+          <span>
+            {"100"}
+          </span>
+          <span>{"'"}</span>
+          <span>{" "}</span>
+          <span>{"/>"}</span>
+        </span>
+
+        <span className="ml-[20px]">
+          <span>{"<p>"}</span>
+          <br />
+          <span className="ml-[20px]">{description}</span>
+          <br />
+          <span className="">{"</p>"}</span>
+        </span>
+        <span>{"</div>"}</span>
+      </code>
+
+      <div className="relative w-max h-max mt-[-105px]">
+        <div style={{
+          background: "rgba(255, 255, 255, 0.2)",
+          borderRadius: "16px",
+          boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+          backdropFilter: "blur(5px)",
+          border: "1px solid rgba(255, 255, 255, 0.3)",
+        }} className="w-max h-max p-[20px] rounded-lg flex flex-col items-center text-center">
+          <Image src="/person.png" alt="person" width={100} height={100} className="object-contain" />
+          <p>
+            Let's make it interactive.<br /> See the magic of tailwind.
+          </p>
+        </div>
+
+        {/*...Background...*/}
+        <div style={{
+          zIndex: -1
+        }} className=" absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%] w-full h-full">
+          <div className="relative w-full h-full">
+            <div className="absolute top-[-20px] left-[-20px] w-[60px] h-[60px] rounded-full bg-yellow1 "></div>
+            <div className="absolute bottom-[-20px] right-[-20px] w-[60px] h-[60px] rounded-full bg-yellow1 "></div>
+          </div>
+        </div>
       </div>
     </div>
   );
