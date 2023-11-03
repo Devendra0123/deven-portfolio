f83eff7180bf8fe03b504ffdafa60755c2a28cbb
unknown <devendra13.dc@gmail.com>
2023-11-03 16:50:05 +0545

    working on hire section

--- app/hire/page.tsx
+++ app/hire/page.tsx
@@ -1,5 +1,6 @@
 import TextAnimation from "@/components/Animations/TextAnimation";
 import SliderCard from "@/components/Card/SliderCard";
+import Laptop from "@/components/Elements/Laptop";
 import ShowcaseSlider from "@/components/Slider/ShowcaseSlider";
 
 const Hire = () => {
@@ -10,18 +11,21 @@ const Hire = () => {
         Warm welcome.
       </h1>
 
-      <div className="w-full lg:max-w-[55%] flex justify-center items-center whitespace-normal flex-wrap text-xl mt-[30px] text-center">
+      <div className="w-full lg:max-w-[65%] flex justify-center items-center whitespace-normal flex-wrap text-xl mt-[30px] text-center">
         <p>My name is</p>
         <span className="text-2xl font-bold text-slate-700 pl-[10px]">
           <TextAnimation text="Devendra Chaudhary." />
         </span>
-         I am a professional web developer with good proficiency in
+        I am a professional web developer with good proficiency in
         <span className="pl-[5px] underline underline-offset-8 decoration-primaryBlue font-bold tracking-wider"> React & Next.js.</span> I have been in web development journey
         for more than 2 years.
       </div>
 
-      <div className="w-[90%] overflow-hidden mt-[50px] bg-slate-300 shadow-lg shadow-yellow1/75 p-[20px] flex justify-center">
+      {/* <div className="w-[90%] overflow-hidden mt-[50px] bg-slate-300 shadow-lg shadow-yellow1/75 p-[20px] flex justify-center">
         <ShowcaseSlider />
+      </div> */}
+      <div className="mt-[50px]">
+      <Laptop />
       </div>
 
     </div>
---
+++ components/Elements/Laptop.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Laptop() {
+  return (
+    <div className="laptop">
+      <div className="laptop__screen">
+        <img src="https://picsum.photos/1600/1000" alt="Screen" />
+      </div>
+      <div className="laptop__bottom"></div>
+      <div className="laptop__under"></div>
+      <div className="laptop__shadow"></div>
+    </div>
+  );
+}
+
+export default Laptop;
