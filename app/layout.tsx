5a01091e430ad0193fb8a7f5a4f7d7c2da17b7b3
Devendra0123 <88467890+Devendra0123@users.noreply.github.com>
2024-07-19 10:33:21 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Navbar from "@/components/Header/Navbar";
 import MessageSection from "@/components/Sections/MessageSecton";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,6 +20,41 @@ export default function RootLayout({
         <Navbar />
         {children}
         <MessageSection />
+        <Script
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+    const iframe = document.createElement("iframe");
+    const iframeStyles = (styleString) => {
+    const style = document.createElement('style');
+    style.textContent = styleString;
+    document.head.append(style);
+    }
+    
+    iframeStyles('
+        .chat-frame {
+            position: fixed;
+            bottom: 50px;
+            right: 50px;
+            border: none;
+        }
+    ')
+    
+    iframe.src = "https://deven-chatbot.vercel.app/chatbot"
+    iframe.classList.add('chat-frame')
+    document.body.appendChild(iframe)
+    
+    window.addEventListener("message", (e) => {
+        if(e.origin !== "https://deven-chatbot.vercel.app") return null
+        let dimensions = JSON.parse(e.data)
+        iframe.width = dimensions.width
+        iframe.height = dimensions.height
+        iframe.contentWindow.postMessage("94ee4e80-1eb6-4907-8533-654467eed2af", "https://deven-chatbot.vercel.app/")
+    })
+        
+  `,
+  }}
+/>
       </body>
     </html>
   );
