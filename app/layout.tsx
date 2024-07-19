dba9a6c3b0d961eef2a0409fd751e0abe1d309df
Devendra0123 <88467890+Devendra0123@users.noreply.github.com>
2024-07-19 09:44:13 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,40 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-          <script>
-
-    const iframe = document.createElement("iframe");
-
-    const iframeStyles = (styleString) => {
-      const style = document.createElement('style');
-      style.textContent = styleString;
-      document.head.append(style);
-    }
-
-    iframeStyles(`
-        .chat-frame {
-            position: fixed;
-            bottom: 50px;
-            right: 50px;
-            border: none;
-        }
-    `)
-
-    iframe.src = "https://deven-chatbot.vercel.app/chatbot"
-    iframe.classList.add('chat-frame')
-    document.body.appendChild(iframe)
-
-    window.addEventListener("message", (e) => {
-      if (e.origin !== "https://deven-chatbot.vercel.app") return null
-      let dimensions = JSON.parse(e.data)
-      iframe.width = dimensions.width
-      iframe.height = dimensions.height
-      iframe.contentWindow.postMessage("94ee4e80-1eb6-4907-8533-654467eed2af", "https://deven-chatbot.vercel.app/")
-    })
-
-  </script>
-      </head>
       <body className="relative w-full overflow-x-hidden bg-[#F0F7FF] py-[30px] font-poppins flex flex-col items-center">
         <Navbar />
         {children}
